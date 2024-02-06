d5c20ffea158bc1e3525f8760befe1d207f1b073
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-02-06 20:16:56 +0200

    edit page title and logo and add dummy data

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -7,16 +7,9 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: taskview
+      component: taskview,
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+
   ]
 })
 
